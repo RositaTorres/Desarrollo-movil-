6758041f7aeecfe0a6f98958d559d82bf98d018b
Juan Gil <gilj06@hotmail.com>
2022-10-29 03:28:55 +0200

    creacion de pages_POI

---
+++ flutter_application_1/lib/pages/pages_POI/info_general_6.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfoGeneral6 extends StatefulWidget {
+  const InfoGeneral6({super.key});
+
+  @override
+  State<InfoGeneral6> createState() => _InfoGeneral6State();
+}
+
+class _InfoGeneral6State extends State<InfoGeneral6> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

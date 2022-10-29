2701716449d0791cd5fc37e9f3ef286c2fc2a72d
Juan Gil <gilj06@hotmail.com>
2022-10-29 03:12:01 +0200

    creacion de las pantallas POI

---
+++ flutter_application_1/lib/pages/info_general_7.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InfoGeneral7 extends StatefulWidget {
+  const InfoGeneral7({super.key});
+
+  @override
+  State<InfoGeneral7> createState() => _InfoGeneral7State();
+}
+
+class _InfoGeneral7State extends State<InfoGeneral7> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
